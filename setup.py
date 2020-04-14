f09df8d1057e54adefb8ca4da5b0877d3d165298
William Short <w.short@exeter.ac.uk>
2020-04-14 21:26:42 +0100

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-version = "0.0.4"
+version = "0.0.5"
 
 requirements = ["cylleneus>=0.3.7", "Flask>=1.1.1", "html3>=1.17", "peewee>=3.13.1"]
 
