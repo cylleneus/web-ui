879555e3669c4fd35d50a4ec9bfdbcffb45aee4d
ws308 <w.short@exeter.ac.uk>
2020-04-22 20:14:44 +0100

    UI improvements

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-version = "0.0.7"
+version = "0.0.8"
 
-requirements = ["cylleneus>=0.5.1", "Flask>=1.1.1", "html3>=1.17", "peewee>=3.13.1"]
+requirements = ["cylleneus>=0.5.2", "Flask>=1.1.1", "html3>=1.17", "peewee>=3.13.1"]
 
 setup_requirements = []
 
